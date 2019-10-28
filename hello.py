94630b9b353d3e0eff9d55b5f60993585d6ee69d
AIXPact <frank@aixpact.com>
2019-10-28 15:06:54 +0100

    commit r.content

--- hello.py
+++ hello.py
@@ -13,4 +13,4 @@ def index():
     r = requests.get('http://www.aixpact.ml/api/httptrigger?name=frank')
     # print(r.content, r.status_code)
 
-    return jsonify({'name':r.content})
+    return r.content #jsonify({'name':r.content})
