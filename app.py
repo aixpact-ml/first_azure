0fa01926dfe96ed10447be3bd2a7c3f265f93e59
AIXPact <frank@aixpact.com>
2019-10-28 14:39:21 +0100

    commit jsonify

--- app.py
+++ app.py
@@ -6,7 +6,9 @@
 # gunicorn --bind=0.0.0.0 --timeout 600 app:app
 # # If hello.py
 # gunicorn --bind='0.0.0.0' --timeout=600 hello:app
-from hello import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8241)
+# For local debugging
+# from hello import app
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True, port=8241)
