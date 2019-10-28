317e1ecca73f2e63451ae2f06626778d40cba570
AIXPact <frank@aixpact.com>
2019-10-28 13:33:51 +0100

    commit by sh

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 # gunicorn --bind=0.0.0.0 hello:app
 # --timeout 600
+
+# # If application.py
+# gunicorn --bind=0.0.0.0 --timeout 600 application:app
+# # If app.py
+# gunicorn --bind=0.0.0.0 --timeout 600 app:app
+# # If hello.py
+# gunicorn --bind=0.0.0.0 --timeout 600 hello:app
