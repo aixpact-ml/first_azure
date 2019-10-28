0210f6ed07d07b20c83be6c8bc3462f4a7aaf693
AIXPact <frank@aixpact.com>
2019-10-28 13:55:51 +0100

    commit hello aixpact

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
+import requests
 from flask import Flask
+from flask import (Blueprint, redirect, request, flash, url_for, jsonify,
+                   render_template, session, current_app, make_response)
 
 app = Flask(__name__)
 
@@ -6,4 +9,5 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """"""
-    return "<h1>Hello aiXpact!</h1>"
+    api_ = requests.get'http://www.aixpact.ml/api/httptrigger?name=frank')
+    return f"<h1>Hello {api_}!</h1>"
