d0a65d1efb42108e038470ff84195a91fdf8f3bc
AIXPact <frank@aixpact.com>
2019-10-28 14:12:41 +0100

    commit json

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import Flask
 from flask import (Blueprint, redirect, request, flash, url_for, jsonify,
                    render_template, session, current_app, make_response)
