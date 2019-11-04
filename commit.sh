c02a02cd943599cbefb2c169ba53c642f44fd0e7
AIXPact <frank@aixpact.com>
2019-11-04 09:25:36 +0100

    clean commit

--- commit.sh
+++ commit.sh
@@ -5,3 +5,8 @@ commit_message="$1"
 git add . -A
 git commit -m "$commit_message"
 git push -u origin master
+
+
+# delete remote file and keep local file
+# git rm --cached somefile.ext
+# git rm --cached Dockerfile *.yml
