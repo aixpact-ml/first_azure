656cf38fbc4acec668945e5bc9a157bad276f432
AIXPact <frank@aixpact.com>
2019-11-04 21:38:44 +0100

    secret vault import fix

--- hello.py
+++ hello.py
@@ -148,7 +148,7 @@ def run_function():
 @app.route("/secrets")
 def secrets():
     # from azure.keyvault import KeyVaultClient
-    from azure.common.credentials import ServicePrincipalCredentials
+    # from azure.common.credentials import ServicePrincipalCredentials
     from azure.identity import DefaultAzureCredential
     from azure.keyvault.secrets import SecretClient
 
