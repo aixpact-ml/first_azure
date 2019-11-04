09f05cb7f46f52775d5760d2328432dcd2dedec1
AIXPact <frank@aixpact.com>
2019-11-04 16:59:18 +0100

    config fix

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
 import pandas as pd
 
 
-creds = pd.read_pickle('creds.pkl')
+# creds = pd.read_pickle('creds.pkl')
 
-class Config(object):
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
+# class Config(object):
+#     def __init__(self, **entries):
+#         self.__dict__.update(entries)
 
-config = Config(creds)
+# config = Config(creds)
 
 # class Config(object):
 #     def __init__(self, pickl='creds.pkl'):
 #         credentials = pd.read_pickle(pickl)
 #         for k, v in credentials.items():
 #             setattr(self, k, v)
+
+class Config(object):
+    pass
+
+config = Config()
+config.__dict__ = pd.read_pickle('creds.pkl')
