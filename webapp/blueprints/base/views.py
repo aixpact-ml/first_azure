409101d3aed11401806a4f6f2f7b5fe22f0519d6
AIXPact <frank@aixpact.com>
2019-11-06 13:02:37 +0100

    templates folder

--- webapp/blueprints/base/views.py
+++ webapp/blueprints/base/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import logging
 import datetime
 import requests
@@ -52,19 +53,35 @@ def upload_form():
                 # Azure
                 file.save(file_in)
                 block_blob(file_in, config.BLOB_CONX)
-        send_email(email, file_in)
-        flash(f'thank you an email has been sent to: {email} (attachment: {file_in})')
-        return redirect(url_for('base_blueprint.thankyou', message=file_in, email=email))
+
+        return redirect(url_for('base_blueprint.thankyou',
+            message=file_in,
+            email=email,
+            function_name='HttpTrigger'))
         flash(f'Try again.....')
     return render_template('base/upload.html', form=form)
 
 
 @blueprint.route("/thankyou")
 def thankyou():
+    file_in = request.args.get('file_in')
+    email = request.args.get('email')
+    function_name = request.args.get('function_name')
+    file_out = f'{function_name}_file_in'
+    url = f'http://www.aixpact.ml/api/{function_name}'
+
+    response = requests.post(url, files={'file': open(file_in, 'rb')})
+    df = pd.read_csv(response.text, header=0)
+    df.to_csv(file_out)
+
+    send_email(email, file_out)
+    flash(f'thank you an email has been sent to: {email} (attachment: {file_out})')
+
+    # return # jsonify(response=response.content)
     try:
         r = render_template('base/thankyou.html',
             email=request.args.get('email'),
-            file=request.args.get('email'))
+            file=request.args.get('file_in'))
         return r
     except:
         return jsonify(status='succes', response=request.args.get('message'))
