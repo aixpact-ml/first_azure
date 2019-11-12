d371ed083dc9662a19f944e2f6e90cf104fb0e8a
AIXPact <frank@aixpact.com>
2019-11-12 22:57:30 +0100

    upload form client side custom alerts

--- webapp/blueprints/base/forms.py
+++ webapp/blueprints/base/forms.py
@@ -18,9 +18,32 @@ class FileForm(FlaskForm):
     function = SelectField('API name', choices=functions, validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+    # TODO in-field validation
+    def __init__(self, *args, **kwargs):
+        super(FileForm, self).__init__(*args, **kwargs)
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    def validate(self):
+        initial_validation = super(FileForm, self).validate()
+        if not initial_validation:
+            return False
+
+        if not self.file:
+            self.file.errors.append('Please select file.')
+            return False
+
+        if not self.email:
+            self.email.errors.append('Please enter email.')
+            return False
+
+        if not self.function:
+            self.function.errors.append('Please choose function.')
+            return False
+
+        return True
+
+
+# class LoginForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     remember_me = BooleanField('Remember Me')
+#     submit = SubmitField('Sign In')
