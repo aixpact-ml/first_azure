1dc8b8f551f1a9682ab0b657ccff5258c478fb63
AIXPact <frank@aixpact.com>
2019-11-12 22:36:40 +0100

    upload form client side custom alerts

--- webapp/blueprints/base/forms.py
+++ webapp/blueprints/base/forms.py
@@ -1,15 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email, InputRequired
 from flask_wtf.file import FileField, FileRequired
+
+
+functions = [('1', 'HttpTrigger'),
+             ('2', 'Hello')]
+
 
 class FileForm(FlaskForm):
     file = FileField('File', validators=[FileRequired()])
     email = EmailField('Email address', validators=[
         InputRequired("Please enter your email address."),
         Email("Please enter your email address.")])
-    function = StringField('API name', validators=[DataRequired()])
+    function = SelectField('API name', choises=functions, validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
