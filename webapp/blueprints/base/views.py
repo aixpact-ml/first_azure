236cdfba35f814e2355a4169a5d072708f2474f9
AIXPact <frank@aixpact.com>
2019-11-12 21:51:58 +0100

    fixed thankyou route and template

--- webapp/blueprints/base/views.py
+++ webapp/blueprints/base/views.py
@@ -18,18 +18,18 @@ from utils.functions import _log_msg, allowed_file, binary2csv, predict
 from utils.email import send_email, mail
 from utils.decorators import fire_and_forget
 
-# from webapp.extensions import db, login_manager, csrf #, mail
 from webapp.extensions import mail
 from . import blueprint
 
 
-def handle_uploaded_file(file, file_dest):
+def handle_uploaded_file(file):
     # Azure vs. local, web form vs. POST
     try:
         os.mkdir('./data')
     except:
         pass
-    file_dest = os.path.join('./data', file_dest)  # was file_in =
+    file_temp = f'temp_in.{file.filename.split(".")[-1]}'
+    file_dest = os.path.join('./data', file_temp)
     try:
         # When file in is raw data
         with open(file_dest, "wb") as dest:
@@ -70,12 +70,7 @@ def debug():
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    # https://stackoverflow.com/questions/38079288/flask-file-not-detecting-on-upload
-    # When using email with file attached
-    # from werkzeug.datastructures import CombinedMultiDict
-    # form = FileForm(CombinedMultiDict((request.files, request.form)))
-
-    # If email without file upload
+    """HttpTrigger"""
     form = FileForm()
 
     # Set csrf token in hidden field - avoid error message
@@ -85,143 +80,33 @@ def index():
     if form.validate_on_submit():
         file = form.file.data
         email = form.email.data
-        name = email.split('@')[0].lower().capitalize()
         function = form.function.data
-        ext = '.' + file.filename.split('.')[-1]
-        file_in = email.replace('@', '_').replace('.', '_').replace('-', '_') + ext
-        file_out = f'{function}_{uuid.uuid4()}.txt'  # random filename
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            assert request.files['file'] == form.file.data, 'wtf'
+
+            # Predict - HttpTrigger
             try:
-                file_dest = handle_uploaded_file(form.file.data, file_in)
+                file_dest = handle_uploaded_file(file)
+                blob_uri = predict(file_dest, email, function)
             except Exception as err:
-                return jsonify(status='failed',
-                               error=err,
-                               file=str(form.file.data),
-                               data=str(open(file_dest, 'rb')))
-
-
-            # print('DEBUG', type(file), '\n', request.files['file']) #, isinstance(file))
-            # logging.info('DEBUG', type(file), '\n', request.files["file"]) #, isinstance(file))
-            # assert 1 == 0, f'{request.files["file"]}'
-            # # local: DEBUG <class 'werkzeug.datastructures.FileStorage'>
-            # # Azure: DEBUG <class
-
-            # # Send email
-            # try:
-            #     download = f'https://helloaixpact.blob.core.windows.net/hapidays/{file_out}'
-            #     template = render_template('base/email_message.html',
-            #                                 name=name,
-            #                                 filename=download)
-            #     send_email(template, email, download)
-            # except Exception as err:
-            #     print('email error:', err)
-
-            # Call serverless function
-            # data = predict(file_dest, file_out, function)
-            # data = predict(open(file_dest, 'rb').read(), file_out, function)  # test file handle
-
-            # Call serverless Azure function - returns blob_id
-            blob_uri = predict(file_dest, email, function)
-            # url = f'https://hello-aixpact.azurewebsites.net/api/{function}'
-            # files = {'file': open(file_dest, 'rb')}
-            # response = requests.post(url, files=files)
-            # blob_uri = f'https://helloaixpact.blob.core.windows.net/hapidays/{response.text}'
-
-            # Send email
-            # try:
-            # template = render_template('base/email_message.html',
-            #                             name=name,
-            #                             filename=blob_uri)
-            #     send_email(template, email, blob_uri)
-            # except Exception as err:
-            #     print('email error:', err)
-
-            return jsonify(status='succes',
-                           file=str(form.file.data),
-                           data=str(open(file_dest, 'rb').read()[:100]),
-                           )
-
-            # return jsonify(status='succes',
-            #                file=str(form.file.data),
-            #                data=str(open(file_dest, 'rb').read()[:100]),
-            #                reponse_status=response.status_code,
-            #                reponse_reason=response.reason,
-            #                predict=str(response.text[:100]))  ###### was os.path....
-
+                return jsonify(status='failed', error=str(err))
         else:
-            return render_template('base/upload.html', form=form)  # TODO
+            return render_template('base/upload.html', form=form)
 
-        # TODO simpify thankyou
-        return redirect(url_for('base_blueprint.thankyou',
-                        file_in=file_in,
-                        file_out=file_out,
-                        email=email,
-                        function=function,
-                        data=data[:10]))  # Check/debug data from within Azure
-        flash(f'Try again.....')
+        return redirect(url_for('base_blueprint.thankyou', name=email.split('@')[0].lower().capitalize()))
+
     return render_template('base/upload.html', form=form)
 
 
 @blueprint.route("/thankyou")
 def thankyou():
-    file_in = request.args.get('file_in')
-    email = request.args.get('email')
-    function = request.args.get('function')
-    file_out = request.args.get('file_out')
-    data = request.args.get('data')
-
-    flash(f'Thank you! \n\nAn email has been sent to: {email} \nAttachment: {file_out}')
-    try:
-        r = render_template('base/thankyou.html',
-                            email=email.split('@')[0],
-                            file=file_out)
-        return r
-    except:
-        return jsonify(status='succes',
-                       response=f'Thank you! \n\nAn email has been sent to: {email} \n \
-                                  Attachment: {file_out}',
-                       data=data)  # Check/debug data from within Azure
+    name = request.args.get('name')
+    return render_template('base/thankyou.html', name=name)
 
 
 @blueprint.route("/function/<function_name>")
 def function(function_name):
     """Takes some time....."""
     response = requests.get(f'http://www.aixpact.ml/api/{function_name}')
-    return response.text  # jsonify(response=response.content)
-
-
-################ OLD
-# @blueprint.route('/upload_form', methods=['GET', 'POST'])
-# def upload_form():
-#     form = FileForm()
-#     if form.validate_on_submit():
-#         file = form.file.data
-#         email = form.email.data
-#         function_ = form.function.data
-#         print('DEBUG function:', form.function.data)
-#         ext = '.' + file.filename.split('.')[-1]
-#         file_in = email.replace('@', '_').replace('.', '_').replace('-', '_') + ext
-
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             try:
-#                 # Local dev
-#                 file.save(os.path.join(config.LOCAL, file_in))
-#             except:
-#                 # Azure
-#                 file.save(file_in)
-#                 # blob_upload(file_in)
-#                 # block_blob(file_in, config.BLOB_CONX)
-#         else:
-#             return render_template('base/upload.html', form=form)  # TODO
-
-#         # assert file_in is None, f'file_in: {file_in} \n {open(file_in, "rb")}\n'
-#         assert function_ is None, f'function is {function_}'
-#         return redirect(url_for('base_blueprint.thankyou',
-#                         file_in=file_in,
-#                         email=email,
-#                         function_=function_))
-#     return render_template('base/upload.html', form=form)
+    return response.text
